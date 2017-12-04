c74d7278c5065fea88d7d18c016cf1b8e9f55a07
gihan herath <gihan.h@hotmail.com>
2017-12-04 18:07:04 +0530

    revert to version 6.1.0.17

--- subscriptions/subscriptions.module.js
+++ subscriptions/subscriptions.module.js
@@ -1,9 +1,9 @@
 ////////////////////////////////
 // App : Subscription
 // Owner  : Gihan Herath
-// Last changed date : 2017/11/17
-// Version : 6.1.0.19
-// Modified By : Kasun
+// Last changed date : 2017/10/30
+// Version : 6.1.0.17
+// Modified By : Gihan
 /////////////////////////////////
 
 (function ()
@@ -36,8 +36,8 @@
 
 			  return $q(function(resolve, reject) {
 				  $timeout(function() {
-					  //if (true) {
-					  if ($rootScope.isBaseSet2) {
+					  if (true) {
+					  //if ($rootScope.isBaseSet2) {
 						  resolve(function () {
 							  mesentitlementProvider.setStateCheck("subscriptions");
 
