4f5d1a6c3aad465b748281b190cbf6fbe5d21983
gihan herath <gihan.h@hotmail.com>
2018-04-16 13:15:41 +0530

    add new profile error message handled

--- subscriptions/subscriptions.module.js
+++ subscriptions/subscriptions.module.js
@@ -1,7 +1,7 @@
 ////////////////////////////////
 // App : Subscription
 // Owner  : Gihan Herath
-// Last changed date : 2018/01/09
+// Last changed date : 2018/04/16
 // Version : 6.1.0.20
 // Modified By : Gihan
 /////////////////////////////////
